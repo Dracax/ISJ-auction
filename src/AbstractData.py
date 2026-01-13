9fdcbfae7cdb3a62233610b54f3061ff3d47021f
Samuel Heid <sjheid@t-online.de>
2026-01-13 21:21:26 +0100

    let the abstract data hold the type of the request

--- src/AbstractData.py
+++ src/AbstractData.py
@@ -1,2 +1,26 @@
+from dataclasses import dataclass, field
+from enum import Enum
+
+
+class DataType(Enum):
+    BROADCAST_ANNOUNCE_REQUEST = "BroadcastAnnounceRequest"
+    BROADCAST_ANNOUNCE_RESPONSE = "BroadcastAnnounceResponse"
+    MULTICAST_GROUP_RESPONSE = "MulticastGroupResponse"
+    AUCTION_RETRIEVE_REQUEST = "AuctionRetrieveRequest"
+
+
+@dataclass
 class AbstractData:
-    pass
+    data_type: DataType = field(init=False)
+
+
+DATA_TYPE_REGISTRY: dict[DataType, type[AbstractData]] = {}
+
+
+def register_data_type(type_name: DataType):
+    def decorator(cls: type[AbstractData]) -> type[AbstractData]:
+        DATA_TYPE_REGISTRY[type_name] = cls
+        cls.data_type = type_name
+        return cls
+
+    return decorator
--- src/request/AbstractRequest.py
+++ src/request/AbstractRequest.py
@@ -5,5 +5,4 @@ from AbstractData import AbstractData
 
 @dataclass
 class AbstractRequest(AbstractData):
-    request_type: str  # e.g., "AUCTION_RETRIEVE", "BID_PLACEMENT"
     request_address: tuple[str, int]
